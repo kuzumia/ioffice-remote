57ff0870bcbf1d54df6b2866bb8c5dee7da75f14
kuzumia <140256255+kuzumia@users.noreply.github.com>
2026-07-02 04:09:37 +0700

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'ioffice-remote-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // Bỏ qua các API fetch (Github API) không cache để luôn lấy dữ liệu mới
+  if (event.request.url.includes('api.github.com')) {
+    return;
+  }
+  
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
